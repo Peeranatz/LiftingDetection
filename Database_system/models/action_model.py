045b5966b3ed545a9cd2231ccb4a0b4f6416f963
Peeranatz <peeranat280648@gmail.com>
2025-06-23 19:09:35 +0700

    Add object_id for action_model

--- Database_system/models/action_model.py
+++ Database_system/models/action_model.py
@@ -3,9 +3,10 @@ from mongoengine import Document, StringField, DateTimeField, connect
 connect("activity_db", host="localhost", port=27017)
 
 class Action(Document):
-    person_id = StringField(required=True)  # เพิ่ม person_id
+    person_id = StringField(required=True)
     action = StringField(required=True)
-    object_type = StringField(required=False)  # เพิ่ม object_type (optional)
+    object_type = StringField(required=False)
+    object_id = StringField(required=False)  # เพิ่ม object_id (optional)
     start_time = DateTimeField(required=True)
     end_time = DateTimeField(required=True)
     created_at = DateTimeField(required=True)
