42d042508d3c1e5a0b89c42f6d5d8fe0e2153a6f
Peeranatz <peeranat280648@gmail.com>
2025-06-23 19:10:19 +0700

    Add object_id for log_to_mongo

--- Database_system/log_to_mongo.py
+++ Database_system/log_to_mongo.py
@@ -1,14 +1,15 @@
 from datetime import datetime
 from models.action_model import Action
 
-def log_action(person_id, action, start_time, end_time, object_type=None):
+def log_action(person_id, action, start_time, end_time, object_type=None, object_id=None):
     act = Action(
         person_id=person_id,
         action=action,
         object_type=object_type,
+        object_id=object_id,  # เพิ่ม object_id
         start_time=start_time,
         end_time=end_time,
         created_at=datetime.utcnow(),
     )
     act.save()
-    print("✅ Logged Action:", person_id, action, object_type)
+    print("✅ Logged Action:", person_id, action, object_type, object_id)
