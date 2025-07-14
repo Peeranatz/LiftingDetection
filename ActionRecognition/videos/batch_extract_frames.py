147c3b3e6972dd33ccf196e15850283ab0c6bb1e
Kankavee <kankaveeramsri22@gmail.com>
2025-07-14 15:05:22 +0700

    extract_frames

--- ActionRecognition/videos/batch_extract_frames.py
+++ ActionRecognition/videos/batch_extract_frames.py
@@ -1,34 +1,41 @@
 import os
-import subprocess
+import cv2
 
-def extract_frames(video_path, output_dir):
-    os.makedirs(output_dir, exist_ok=True)
-    output_pattern = os.path.join(output_dir, "frame_%03d.jpg")
-
-    cmd = ["ffmpeg", "-i", video_path, output_pattern]
-
-    try:
-        subprocess.run(cmd, check=True)
-        print(f"✅ Extracted: {video_path} → {output_dir}")
-    except subprocess.CalledProcessError:
-        print(f"[ERROR] Failed: {video_path}")
-
-def process_all_videos(input_root="/Users/balast/Desktop/LiftingProject/LiftingDetection/video_datasets_2", output_root="extracted_frames"):
+def build_sequences(input_root="/Users/balast/Desktop/LiftingProject/LiftingDetection/ActionRecognition/videos/extracted_frames", output_root="final_dataset", seq_len=15, step=5):
     for action in os.listdir(input_root):
         action_dir = os.path.join(input_root, action)
         if not os.path.isdir(action_dir):
             continue
 
-        for fname in os.listdir(action_dir):
-            if not fname.lower().endswith((".mp4", ".avi", ".mov", ".mkv")):
+        for clip in os.listdir(action_dir):
+            clip_dir = os.path.join(action_dir, clip)
+            if not os.path.isdir(clip_dir):
                 continue
 
-            video_path = os.path.join(action_dir, fname)
-            clip_name = os.path.splitext(fname)[0]
-            output_dir = os.path.join(output_root, action, clip_name)
+            images = sorted([
+                f for f in os.listdir(clip_dir)
+                if f.lower().endswith((".jpg", ".jpeg", ".png"))
+            ])
 
-            extract_frames(video_path, output_dir)
+            total_frames = len(images)
+            seq_count = 0
+            
+            print(f"Found {total_frames} frames in {clip_dir}")
+
+            for i in range(0, total_frames - seq_len + 1, step):
+                seq_images = images[i:i+seq_len]
+                seq_folder = os.path.join(output_root, action, clip, f"seq{seq_count:04d}")
+                os.makedirs(seq_folder, exist_ok=True)
+
+                for j, img_name in enumerate(seq_images):
+                    src_path = os.path.join(clip_dir, img_name)
+                    dst_path = os.path.join(seq_folder, f"{j:03d}.jpg")
+                    img = cv2.imread(src_path)
+                    cv2.imwrite(dst_path, img)
+
+                seq_count += 1
+
+            print(f"✅ {action}/{clip} → {seq_count} sequences")
 
 if __name__ == "__main__":
-    process_all_videos()
-
+    build_sequences()
