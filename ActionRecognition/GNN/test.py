50cf063dbe4f3a182597f7d21c9cdb5312c21a1b
Kankavee <kankaveeramsri22@gmail.com>
2025-09-25 17:15:41 +0700

    Add FPS

--- ActionRecognition/GNN/test.py
+++ ActionRecognition/GNN/test.py
@@ -1,8 +1,9 @@
 from ultralytics import YOLO
 import yaml
 import cv2
+import time
 
-with open("/Users/balast/Desktop/LiftingProject/LiftingDetection/ActionRecognition/GNN/config.yaml", "r") as f:
+with open("/Users/balast/Desktop/Desktop - All file/LiftingProject/LiftingDetection/ActionRecognition/GNN/config.yaml", "r") as f:
     config = yaml.safe_load(f)
     
 MODEL_PATH = config['HUMAN_MODEL_PT']
@@ -41,7 +42,8 @@ next_local_id = 1
 if not cap.isOpened():
     print("Failed to open video: {}".format(VIDEO_PATH))
     exit()
-
+    
+prev_time = time.time()
 
 while True:
     success, frame = cap.read()
@@ -51,12 +53,19 @@ while True:
     
     frame_count += 1
     
+    # คำนวน FPS 
+    curr_time = time.time()
+    fps = 1.0 / (curr_time - prev_time)
+    prev_time = curr_time
+    
     results = model.track(source=frame, task="pose", persist=True, verbose=False)
     result = results[0]
     
     frame_clean = result.orig_img.copy()     # เฟรมดิบ
     frame_auto_annotated = result.plot()     # YOLO วาดให้เรียบร้อย
     
+    cv2.putText(frame_clean, f"FPS: {fps:.2f}", (10, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
     print(f"Frame: {frame_count}", end='\r')
     for i, box in enumerate(result.boxes):
@@ -96,14 +105,14 @@ while True:
             
             if result.keypoints is not None:
                 keypoints = result.keypoints.xy[i]
-                print("Keypoints (x,y):")
+                # print("Keypoints (x,y):")
                 for idx, (x, y) in enumerate(keypoints):
                     cv2.circle(frame_clean, (int(x), int(y)), 3, (0, 0, 255), -1)
                     name = KEYPOINT_NAMES[idx] if idx < len(KEYPOINT_NAMES) else f"Point{idx}"
-                    print(f"{name}: ({x:.1f}, {y:.1f})")
+                    # print(f"{name}: ({x:.1f}, {y:.1f})")
 
     
-    cv2.imshow("YOLOv11 Pose Detection", frame_auto_annotated)
+    cv2.imshow("YOLOv11 Pose Detection", frame_clean)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         print("Exit by user.")
